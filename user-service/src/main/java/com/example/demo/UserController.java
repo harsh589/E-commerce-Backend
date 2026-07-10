4d591d98c16edf369c2b469ae5c33079aac01511
Yadav <2376688@cognizant.com>
2026-07-10 13:12:07 +0530

    updated new code

--- user-service/src/main/java/com/example/demo/UserController.java
+++ user-service/src/main/java/com/example/demo/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +48,11 @@ public class UserController {
 	    public UserModel getById(@PathVariable Long id) {
 	        return service.getById(id);
 	    }
+	 
+	 @DeleteMapping("/{id}")
+	 public String deleteUser(@PathVariable Long id) {
+		 return service.deleteUser(id);
+		 
+	 }
 	
 }
